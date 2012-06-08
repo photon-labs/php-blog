7cd75736f43c976355e6e8e627e63bdbaa7fff38
Sudhakar <sudhakar.raghunathan@photoninfotech.net>
2012-06-08 12:44:38 +0530

    xml decryption changes

--- source/config/config.php
+++ source/config/config.php
@@ -1,12 +1,38 @@
+<?php /*
+ * ###
+ * Archetype - phresco-php-archetype
+ * 
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */ ?>
 <?php
+
 function getConfigByName($currentEnv, $type, $name) {
-	$fileContents = file_get_contents("sites/default/config/phresco-env-config.xml");
+    $self = $_SERVER['PHP_SELF'];
+    $deploydir = explode("/", $self);
+
+    $host = "http://".$_SERVER["HTTP_HOST"]."/".$deploydir[1]."/";
+	
+	$fileContents = file_get_contents($host.'config/phresco-env-config.xml');
 	$file = getOriginalString($fileContents);
 	
 	$document = new DOMDocument();
 	$document->loadXML($file);
 	$xmlDoc = $document->documentElement;
 	$xml = simplexml_load_string($file);
+
 	foreach ($xmlDoc->childNodes AS $envNode) {
 		if ($envNode->nodeName == "environment") {
 			$env = $envNode->getAttribute("name");
