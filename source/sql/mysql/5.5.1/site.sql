6d16987c8603d57cd7737b9b1015eeabb9943b0a
saravanan-na <saravanan.natarajan@photoninfotech.net>
2012-06-19 16:18:26 +0530

    [artf481854] Database not created for php eshop project(1.2.3000) issue fixed

--- source/sql/mysql/5.5.1/site.sql
+++ source/sql/mysql/5.5.1/site.sql
@@ -1,4 +1,4 @@
----
+/*---
 -- ###
 -- PHR_PhpBlog
 -- %%
@@ -16,7 +16,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 -- ###
----
+---*/
 -- phpMyAdmin SQL Dump
 -- version 3.3.9
 -- http://www.phpmyadmin.net
