f9a5ec2d59f6baefb3c06012206bd3b649bd084d
chelladurai_a <chelladurai.ayyanar@photoninfotech.net>
2013-01-24 11:30:15 +0530

    [artf612973] modified testcases

--- test/functional/src/main/java/com/photon/phresco/Screens/BaseScreen.java
+++ test/functional/src/main/java/com/photon/phresco/Screens/BaseScreen.java
@@ -85,20 +85,10 @@ public class BaseScreen {
 			log.info("-------------***LAUNCHING GOOGLECHROME***--------------");
 			try {
 
-				/*
-				 * chromeService = new ChromeDriverService.Builder()
-				 * .usingChromeDriverExecutable( new File(getChromeLocation()))
-				 * .usingAnyFreePort().build(); log.info(
-				 * "-------------***LAUNCHING GOOGLECHROME***--------------");
-				 * chromeService.start();
-				 */
+				
 				capabilities = new DesiredCapabilities();
 				capabilities.setBrowserName("chrome");
-				/*
-				 * break; capabilities.setPlatform(Platform)
-				 * capabilities.setPlatform(selectedPlatform); driver = new
-				 * RemoteWebDriver(server, capabilities);
-				 */
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -143,37 +133,10 @@ public class BaseScreen {
 		}
 		driver = new RemoteWebDriver(server, capabilities);
 		driver.get(applicationURL + applicationContext);
-		// driver.manage().window().maximize();
-		// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 	}
 	
-	public  void windowResize()
-	{
-		phrsc = new PhrescoUiConstants();
-		String resolution = phrsc.RESOLUTION;		
-		if(resolution!=null)
-		{
-		String[] tokens = resolution.split("x");
-		String resolutionX=tokens[0];
-		String resolutionY=tokens[1];		
-		int x= Integer.parseInt(resolutionX);
-		int y= Integer.parseInt(resolutionY);
-		Dimension screenResolution = new Dimension(x,y);
-		driver.manage().window().setSize(screenResolution);
-		}
-		else{
-			driver.manage().window().maximize();
-		}
-	}
-	/*
-	 * public static void windowMaximizeFirefox() {
-	 * driver.manage().window().setPosition(new Point(0, 0)); java.awt.Dimension
-	 * screenSize = java.awt.Toolkit.getDefaultToolkit() .getScreenSize();
-	 * Dimension dim = new Dimension((int) screenSize.getWidth(), (int)
-	 * screenSize.getHeight()); driver.manage().window().setSize(dim); }
-	 */
-
+	
 	public void closeBrowser() {
 		log.info("-------------***BROWSER CLOSING***--------------");
 		if (driver != null) {
